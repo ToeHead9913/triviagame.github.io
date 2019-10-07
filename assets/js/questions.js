61ec35fd2b4327d4bf88a79339f2100b9a411db7
Trenton Gharst <toe9913@gmail.com>
2019-10-06 23:49:07 -0500

    stuff

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+const quizQuestions = [
+    {
+        question: 'Which of the following consoles was eleased by "Sega"?',
+        choices: ["Gamecube", "Zodiac", "Jaguar", "Dreamcast"],
+        correctAnswer: "Dreamcast"
+    },
+
+    {
+        question: "Which of the following is NOT a ghost from the original Pac-Man?",
+        choices: ["Winky", "Blinky", "Clyde", "Inky"],
+        correctAnswer: "Winky"
+    },
+
+    {
+        question: "Under what name did Mario debut in 1981's 'Donkey Kong'?",
+        choices: ["Plumberman", "Jumpman", "Mario", "Luigi"],
+        correctAnswer: "Jumpman"
+    },
+   
+    {
+        question: "Which year was Fallout 3 released?",
+        choices: ["2002", "2004", "2006", "2008"],
+        correctAnswer: "2008"
+    },
+
+    {
+        question: "More than a quarter of all Americans over 50 play video games.",
+        choices: ["True", "False"],
+        correctAnswer: "True"
+    },
+
+    {
+        question: "Nolan Bushnell, who founded Atari in 1972, also founded what national restaurant chain?",
+        choices: ["Applebee's", "Chuck E. Cheese's", "Carl's Jr.", "TGI Fridays"],
+        correctAnswer: "Chuck E. Cheese's",
+    },
+
+    {
+        question: "What was the first video arcade game ever commercially produced?",
+        choices: ["Computer Space", "Pong", "Galaxy Game", "Tetris"],
+        correctAnswer: "Computer Space",
+    },
+
+];
+const funImages = [
+    './assets/images/happy fun.gif',
+    './assets/images/hellYeah.gif',
+    './assets/images/winning.gif',
+    './assets/images/yeah.gif',
+    './assets/images/you did it.gif'
+];
+
+const sadImages = [
+    './assets/images/danm.gif',
+    './assets/images/oops.gif',
+    './assets/images/pretty bad.gif'
+];
